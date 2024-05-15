778d09175149f79ba9a3620711105c121222584e
Lê Quang Anh <anhlq@ftech.ai>
2024-05-15 12:00:49 +0700

    aaaa

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
         .library(
             name: "TIOSDKSwift",
             targets: ["TIOSDKSwift"]),
+        .library(name: "TIOSDK", targets: ["TIOSDK"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,13 +20,9 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "TIOSDKSwift",
-            dependencies: ["TIOSDK"],
-            linkerSettings: [
-                .linkedFramework("TIOSDK")
-            ]
-            
+            name: "TIOSDKSwift"
         ),
+        .target(name: "TIOSDK", dependencies: ["TIOSDK"]),
         .testTarget(
             name: "TIOSDKSwiftTests",
             dependencies: ["TIOSDKSwift"]),
